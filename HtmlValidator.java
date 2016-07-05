3ea76debdf0e5603b270065162a45649d175574d
XDLLee <neigemore@163.com>
2016-07-05 15:21:42 -0700

    Add files via upload

--- HtmlValidator.java
+++ HtmlValidator.java
@@ -1,13 +1,18 @@
 import java.util.*;
 
 public class HtmlValidator {
-   private Queue<HtmlTag> validator;
+   private Queue<HtmlTag> validator; //
    public final String INDENTATION = "    ";
    
+   //Construct a new empty validating Queue
    public HtmlValidator() {
       validator = new LinkedList<HtmlTag>();
    }
    
+   //pre : Passed Queue is not null;
+   //      Otherwise throw IllegalArgumentException
+   //
+   //post: Construct a validating Queue based on passed Queue storing tags
    public HtmlValidator(Queue<HtmlTag> tag) {
       if (tag == null) {
          throw new IllegalArgumentException();
@@ -19,6 +24,10 @@ public class HtmlValidator {
       }
    }
    
+   //pre : Passed tag is not null;
+   //      Otherwise throw IllegalArgumentException
+   //
+   //post: Add the passed tag to the end of the validating Queue
    public void addTag(HtmlTag tag) {
       if (tag == null) {
          throw new IllegalArgumentException();
@@ -26,6 +35,7 @@ public class HtmlValidator {
       validator.add(tag);
    }
    
+   //Return a copy of the validating Queue
    public Queue<HtmlTag> getTags() {
       Queue<HtmlTag> result = new LinkedList<HtmlTag>();
       for (int i = 0; i < validator.size(); i++) {
@@ -35,6 +45,11 @@ public class HtmlValidator {
       return result;
    }
    
+   //pre : Passed String element is not null;
+   //      Otherwise throw IllegalArgumentException
+   //
+   //post: Remove all tags which excatly match the passed String
+   //      in the validating Queue
    public void removeAll(String element) {
       if (element == null) {
          throw new IllegalArgumentException();
@@ -44,12 +59,27 @@ public class HtmlValidator {
       for (int i = 0; i < size; i++) {
          if (validator.peek().getElement().equals(element)) {
             validator.remove();
-         } else {
+         } 
+         else {
             validator.add(validator.remove());
          }
       }
    }
    
+   //Print out and evaluate stored tags in following rules:
+   //Every tag is displayed in a single line;
+   //
+   //Each time see an opening tag which requires a closing tag,
+   //increase level of Indentation;
+   //
+   //Each time see a closing tag matches nearest opening tag,
+   //decrease level of Indentation,
+   //otherwise print "ERROR unexpected tag";
+   //
+   //Each time see a Self-closing tag which is not an opening tag, 
+   //print "ERROR unexpected tag";
+   //
+   //Print "ERROR unclosed tag" for every unclosed opening tag
    public void validate() {
       Stack<HtmlTag> temp = new Stack<HtmlTag>();
       int size = validator.size();
@@ -59,28 +89,34 @@ public class HtmlValidator {
          if (tag.isSelfClosing()) {
             if (!tag.isOpenTag()) {
                System.out.println(unexpected(tag));
-            } else { // Self-closing tag
+            } 
+            else { // Self-closing && Opening tag
                System.out.println(printLine(countOpenTag, tag));
             }
-         } else if (tag.isOpenTag()) {
+         } 
+         else if (tag.isOpenTag()) {
             temp.push(tag);
             System.out.println(printLine(countOpenTag, tag));
             countOpenTag++;
-         } else { // Closing tag
+         } 
+         else { // Current tag is closing tag
             if (temp.isEmpty() || !tag.matches(temp.peek())) {
                System.out.println(unexpected(tag));
-            } else { // Current tag closes stored tag
+            } 
+            else { // Current tag matches stored opening tag
                temp.pop();
                countOpenTag--;
                System.out.println(printLine(countOpenTag, tag));
             }
          }
+         validator.add(tag);
       }
       while (!temp.isEmpty()) {
          System.out.println(unclosed(temp.pop()));
       }
    }
    
+   //Print tags with passed number of Indentations
    private String printLine(int num, HtmlTag element) {
       String result = "";
       for (int i = 0; i < num; i++) {
@@ -90,10 +126,12 @@ public class HtmlValidator {
       return result;
    }
    
+   //Unexpected error String
    private String unexpected(HtmlTag tag) {
       return "ERROR unexpected tag: " + tag.toString();
    }
    
+   //Unclosed error String
    private String unclosed(HtmlTag tag) {
       return "ERROR unclosed tag: " + tag.toString();
    }
